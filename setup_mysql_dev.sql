32478461bf11cc982238c2978efdb80500dfb274
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2024-03-25 20:15:16 -0400

    added test setup file

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -6,5 +6,5 @@ CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost'
 GRANT ALL PRIVILEGES ON 'hbnb_dev'@'localhost'
     TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT SELECT ON 'performance_schema'.*
-    TO 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+    TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 FLUSH PRIVILEGES;
---
+++ setup_mysql_test.sql
@@ -0,0 +1,12 @@
+-- creates test database --
+
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost'
+    IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON 'hbnb_test'@'localhost'
+    TO 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT SELECT ON 'performance_schema'.*
+    TO 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+FLUSH PRIVILEGES;
+
+
