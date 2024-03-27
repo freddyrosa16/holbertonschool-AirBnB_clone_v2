062a58ebc671619c45e1fd2ae3abe0ee2eab95d2
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2024-03-27 01:06:05 -0400

    finished task 6, still need to debug console.py to make it work appropriately

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,10 +1,10 @@
---- Creates a MySql server ---
+-- Creates a MySql server --
 
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost'
     IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON 'hbnb_dev'@'localhost'
-    TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT SELECT ON 'performance_schema'.*
-    TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.*
+    TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON `performance_schema`.*
+    TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,12 +1,12 @@
 -- creates test database --
 
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost'
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost'
     IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON 'hbnb_test'@'localhost'
-    TO 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT SELECT ON 'performance_schema'.*
-    TO 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_test_db`.*
+    TO 'hbnb_test'@'localhost';
+GRANT SELECT ON `performance_schema`.*
+    TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
 
 
