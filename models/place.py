af65129e874ec17ad554e2587129901ff3579d2a
freddyrosa16 <freddy.rosa16@gmail.com>
2024-03-29 16:15:30 -0400

    Updating place class

--- models/place.py
+++ models/place.py
@@ -1,5 +1,20 @@
+#!/usr/bin/python3
+""" Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy import Table, Column, String, Integer, ForeignKey, Float
+from sqlalchemy.orm import relationship
+from models.city import City
+from os import getenv
+
+if getenv("HBNB_TYPE_STORAGE") == "db":
+    place_amenity = Table("place_amenity", Base.metadata,
+                          Column("place_id", String(60),
+                                 ForeignKey("places.id"),
+                                 primary_key=True, nullable=False),
+                          Column("amenity_id", String(60),
+                                 ForeignKey("amenities.id"),
+                                 primary_key=True, nullable=False))
+
 
 class Place(BaseModel, Base):
     """ A place to stay """
@@ -12,6 +27,39 @@ class Place(BaseModel, Base):
     number_bathrooms = Column(Integer, default=0)
     max_guest = Column(Integer, default=0)
     price_by_night = Column(Integer, default=0)
-    latitude = Column(Float)
-    longitude = Column(Float)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
+    amenity_ids = []
 
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        reviews = relationship("Review", backref='place',
+                               cascade='all, delete, delete-orphan')
+
+        amenities = relationship("Amenity", secondary=place_amenity,
+                                 viewonly=False,
+                                 back_populates="place_amenities")
+    else:
+        @property
+        def reviews(self):
+            """
+            Retrieves the reviews associated with a Place
+            """
+            review = []
+            for k, v in models.storage.all().items():
+                cls = k.split('.')[0]
+                if cls == "Review" and v.place_.id == self.id:
+                    review.append(v)
+            return (review)
+
+        @property
+        def amenities(self):
+            """ Amenities Getter """
+            return self.amenity_ids
+
+        @amenities.setter
+        def amenities(self, obj):
+            """
+            Setter for the amenities id's list
+            """
+            if type(obj).__name__ == "Amenity":
+                self.amenity_ids.append(obj.id)
