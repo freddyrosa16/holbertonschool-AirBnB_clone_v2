1c44a26526bdf348d6d49342a4fb79dbdf2eb27e
freddyrosa16 <freddy.rosa16@gmail.com>
2024-04-09 19:25:12 -0400

    Refactor State class to conditionally define cities property based on HBNB_TYPE_STORAGE environment variable

--- models/state.py
+++ models/state.py
@@ -7,6 +7,7 @@ from models.city import City
 from sqlalchemy import Column
 from sqlalchemy import String
 from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class State(BaseModel, Base):
@@ -15,10 +16,11 @@ class State(BaseModel, Base):
     name = Column(String(128), nullable=False)
     cities = relationship("City", backref="state", cascade="delete")
 
-    @property
-    def cities(self):
-        city_list = []
-        for city in list(models.storage.all(City).values()):
-            if city.state_id == self.id:
-                city_list.append(city)
-        return city_list
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            city_list = []
+            for city in list(models.storage.all(City).values()):
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
