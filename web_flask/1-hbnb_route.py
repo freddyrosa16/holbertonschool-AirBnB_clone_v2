d46874bcd5f904be61e95586c4f009be944c9701
freddyrosa16 <freddy.rosa16@gmail.com>
2024-04-11 08:41:18 -0400

    Add Flask routes for displaying a list of states and cities, and corresponding HTML templates

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
+""" starts the flask app """
 from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello():
-    return "Hello, HBNB!"
+def hello_world():
+    """ returns hello HBNB """
+    return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    return "HBNB"
+    """ returns HBNB """
+    return 'HBNB'
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,29 +1,34 @@
 #!/usr/bin/python3
+""" starts the flask app """
 from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello():
-    return "Hello, HBNB!"
+def hello_world():
+    """ returns hello HBNB """
+    return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    return "HBNB"
+    """ returns HBNB """
+    return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def c(text):
-    return 'C %s' % text.replace('_', ' ')
+def funC(text):
+    """ returns C """
+    return 'C ' + text.replace('_', ' ')
 
 
-@app.route('/python/', strict_slashes=False)
+@app.route('/python/', defaults={'text': "is cool"}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python(text="is cool"):
-    return 'Python %s' % text.replace('_', ' ')
+def pyfun(text):
+    """ returns python """
+    return 'Python ' + text.replace('_', ' ')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
