dee08917737b95337197e7169bd59748c4e88bc2
freddyrosa16 <freddy.rosa16@gmail.com>
2024-04-12 14:51:16 -0400

    Add new Flask routes and corresponding HTML templates for displaying filters and search functionality

--- models/state.py
+++ models/state.py
@@ -1,7 +1,7 @@
-
 #!/usr/bin/python3
 """ State Module for HBNB project """
 import models
+
 from models.base_model import BaseModel
 from models.base_model import Base
 from models.city import City
